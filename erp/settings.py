a42c92e65c3c4c98123c568b3ed38b269cd8df6d
oswardmwambehile <oswardmwambehile@gmail.com>
2026-06-04 12:49:33 +0300

    hellow

--- erp/settings.py
+++ erp/settings.py
@@ -87,11 +87,14 @@ WSGI_APPLICATION = 'erp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'prince_international',
+        'USER': 'prince_international',
+        'PASSWORD': 'StronngPsaward123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
