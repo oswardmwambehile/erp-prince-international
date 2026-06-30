0b1f063198d505cc3f57076029966daecee28215
oswardmwambehile <oswardmwambehile@gmail.com>
2026-06-30 13:32:06 +0300

    numbere added

--- customers/models.py
+++ customers/models.py
@@ -30,7 +30,9 @@ class Customer(models.Model):
     )
 
     phone = models.CharField(
-        max_length=20
+        max_length=20,
+        blank=True,
+        null=True
     )
 
     alternative_phone = models.CharField(
